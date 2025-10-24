e04b3a018f263e0f23aa8b0ffeaed2c7a9741660
Gleb <chibgleb99@gmail.com>
2025-10-24 11:45:36 +0100

    chore: Divided UserDto into dto for write and dto for read

--- system/src/main/java/com/example/system/rest/dto/mapper/UserMapper.java
+++ system/src/main/java/com/example/system/rest/dto/mapper/UserMapper.java
@@ -1,52 +1,23 @@
 package com.example.system.rest.dto.mapper;
 
 import com.example.system.domain.model.User;
-import com.example.system.rest.dto.user.UserDto;
+import com.example.system.rest.dto.user.UserReadResponseDto;
+import com.example.system.rest.dto.user.UserWriteDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends Mappable<User, UserDto> {
+public abstract class UserMapper {
 
-//TODO figure out if I need those mappings
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    public abstract User toEntity(UserWriteDto dto);
 
-//    @Mapping(target = "id", source = "id")
-//    @Mapping(target = "username", source = "username")
-//    @Mapping(target = "email", source = "email")
-//    @Mapping(target = "password", source = "password")
-//    @Mapping(target = "firstName", source = "firstName")
-//    @Mapping(target = "lastName", source = "lastName")
-//    @Mapping(target = "bio", source = "bio")
-//    @Mapping(target = "status", source = "status")
-//    @Mapping(target = "isProfilePrivate", source = "isProfilePrivate")
-//    @Mapping(target = "createdAt", source = "createdAt")
-//    @Mapping(target = "updatedAt", source = "updatedAt")
-//
-//    // These fields are not part of DTO but exist in entity
-//    @Mapping(target = "posts", ignore = true)
-//    @Mapping(target = "groups", ignore = true)
-//    @Mapping(target = "ownedGroups", ignore = true)
-//    @Mapping(target = "friends", ignore = true)
-//    User fromDto(UserDto dto);
+    public abstract UserReadResponseDto toDto(User user);
 
-//    @Mappings({
-//            @Mapping(target = "id", source = "id"),
-//            @Mapping(target = "username", source = "username"),
-//            @Mapping(target = "email", source = "email"),
-//
-//            // Password fields are WRITE_ONLY in DTO; skip mapping back
-//            @Mapping(target = "password", ignore = true),
-//            @Mapping(target = "passwordConfirmation", ignore = true),
-//
-//            @Mapping(target = "firstName", source = "firstName"),
-//            @Mapping(target = "lastName", source = "lastName"),
-//            @Mapping(target = "bio", source = "bio"),
-//            @Mapping(target = "status", source = "status"),
-//            @Mapping(target = "isProfilePrivate", source = "isProfilePrivate"),
-//            @Mapping(target = "createdAt", source = "createdAt"),
-//            @Mapping(target = "updatedAt", source = "updatedAt")
-//    })
-//    UserDto toDto(User entity);
+    public abstract List<UserReadResponseDto> toDto(List<User> users);
 
 }
