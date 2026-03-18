9793b87054ef1ec6a2593c4c7455fa8e71cc4197
Gleb <chibgleb99@gmail.com>
2026-03-18 13:14:34 +0000

    feature: implemented proof key for code exchange challenge method

--- frontend/paxfrontend/src/features/Auth/AuthCallback.tsx
+++ frontend/paxfrontend/src/features/Auth/AuthCallback.tsx
@@ -7,8 +7,15 @@ export const AuthCallback = () => {
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
         const code = params.get("code");
-
+        console.log(code);
         if (!code) return;
+
+        const codeVerifier = localStorage.getItem("pkce_code_verifier");
+
+        if (!codeVerifier) {
+            console.error("Missing PKCE code_verifier");
+            return;
+        }
 
         fetch("http://localhost:8080/realms/pax/protocol/openid-connect/token", {
             method: "POST",
@@ -20,14 +27,16 @@ export const AuthCallback = () => {
                 client_id: "pax-frontend",
                 code,
                 redirect_uri: "http://localhost:3000/auth/callback",
+                code_verifier: codeVerifier,
             }),
         })
             .then(res => res.json())
             .then(data => {
                 localStorage.setItem("access_token", data.access_token);
+                localStorage.removeItem("pkce_code_verifier")
                 navigate("/");
             });
-    }, []);
+    }, [navigate]);
 
     return <div>Signing you in…</div>;
 };
